3053e240f032e6607962231dc098112dbd727984
Harish R M <harishmaryada@outlook.com>
2018-08-22 21:59:46 +0530

    Added NavBar

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import NavBar from './Components/NavBar/NavBar'
+
+class App extends Component {
+  render() {
+    return (
+      <div className="">
+        <p className="">
+          <NavBar></NavBar>
+          Get started.
+        </p>
+      </div>
+    );
+  }
+}
+
+export default App;
